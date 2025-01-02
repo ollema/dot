bf5cd47c1ee71a196dc48c911ba1b52114e62c44
Olle Månsson <31876997+ollema@users.noreply.github.com>
2025-01-02 01:37:49 +0100

    more statusline work

--- nvim/.config/nvim/lua/custom/plugins/lualine.lua
+++ nvim/.config/nvim/lua/custom/plugins/lualine.lua
@@ -1,51 +1,69 @@
+local toggleterm_status = function()
+  return '#' .. vim.b.toggle_number
+end
+
+local toggleterm_extension = {
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { toggleterm_status },
+  },
+  filetypes = { 'toggleterm' },
+}
+
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {
-    options = {
-      icons_enabled = true,
-      theme = 'auto',
-      component_separators = { left = '', right = '' },
-      section_separators = { left = '', right = '' },
-      disabled_filetypes = {
-        statusline = {},
-        winbar = {},
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        icons_enabled = false,
+        component_separators = { left = '|', right = '|' },
+        section_separators = { left = nil, right = nil },
+        disabled_filetypes = {
+          statusline = {
+            'snacks_dashboard',
+          },
+        },
       },
-      ignore_focus = {},
-      always_divide_middle = true,
-      always_show_tabline = true,
-      globalstatus = false,
-      refresh = {
-        statusline = 100,
-        tabline = 100,
-        winbar = 100,
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'filetype' },
+        lualine_y = {
+          {
+            'copilot',
+            -- Default values
+            symbols = {
+              status = {
+                icons = {
+                  enabled = '',
+                  sleep = '',
+                  disabled = '',
+                  warning = '',
+                  unknown = '',
+                },
+              },
+            },
+          },
+        },
+        lualine_z = { 'location' },
       },
-    },
-    sections = {
-      lualine_a = { 'mode' },
-      lualine_b = { 'branch', 'diff', 'diagnostics' },
-      lualine_c = { 'filename' },
-      lualine_x = { 'encoding', 'fileformat', 'filetype' },
-      lualine_y = { 'progress' },
-      lualine_z = { 'location' },
-    },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = { 'filename' },
-      lualine_x = { 'location' },
-      lualine_y = {},
-      lualine_z = {},
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {
-      'lazy',
-      'neo-tree',
-      'oil',
-      'toggleterm',
-      'trouble',
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      extensions = {
+        'lazy',
+        'neo-tree',
+        'oil',
+        toggleterm_extension,
+        'trouble',
+      },
     },
   },
+  'AndreM222/copilot-lualine',
 }
