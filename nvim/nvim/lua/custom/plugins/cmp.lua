a048d21b3b45cb62c6f35091dff658ee0100fea6
Olle Månsson <31876997+ollema@users.noreply.github.com>
2025-01-15 21:12:44 +0100

    update cmp

--- nvim/nvim/lua/custom/plugins/cmp.lua
+++ nvim/nvim/lua/custom/plugins/cmp.lua
@@ -9,39 +9,41 @@ return {
       },
     },
   },
-  'saghen/blink.cmp',
-  dependencies = 'rafamadriz/friendly-snippets',
-  version = '*',
-  event = 'InsertEnter',
-  opts = {
-    keymap = { preset = 'default' },
-    appearance = {
-      nerd_font_variant = 'mono',
-    },
-    completion = {
-      menu = {
-        draw = {
-          treesitter = { 'lsp' },
+  {
+    'saghen/blink.cmp',
+    dependencies = 'rafamadriz/friendly-snippets',
+    version = '*',
+    event = 'InsertEnter',
+    opts = {
+      keymap = { preset = 'default' },
+      appearance = {
+        nerd_font_variant = 'mono',
+      },
+      completion = {
+        menu = {
+          draw = {
+            treesitter = { 'lsp' },
+          },
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+        },
+        ghost_text = {
+          enabled = true,
         },
       },
-      documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 200,
-      },
-      ghost_text = {
-        enabled = true,
-      },
-    },
-    sources = {
-      default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
-      providers = {
-        lazydev = {
-          name = 'LazyDev',
-          module = 'lazydev.integrations.blink',
-          score_offset = 100,
+      sources = {
+        default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+        providers = {
+          lazydev = {
+            name = 'LazyDev',
+            module = 'lazydev.integrations.blink',
+            score_offset = 100,
+          },
         },
       },
     },
+    opts_extend = { 'sources.default' },
   },
-  opts_extend = { 'sources.default' },
 }
