3bc51d4bd47df27fb82e1e13e69a09333000fb97
Olle Månsson <31876997+ollema@users.noreply.github.com>
2025-01-23 22:48:17 +0100

    update config

--- nvim/nvim/lua/config/options.lua
+++ nvim/nvim/lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+
+-- disable snacks animations
+vim.g.snacks_animate = false
+
+-- use fish as the shell for the integrated terminal
 vim.opt.shell = "fish"
+
+-- use inline suggestions instead of completion engine
+vim.g.ai_cmp = false
+
+-- use basedpyright instead of pyright
+vim.g.lazyvim_python_lsp = "basedpyright"
+-- use ruff instead of ruff_lsp
+vim.g.lazyvim_python_ruff = "ruff"
