c2db0086b154b3ea76e8c5469d371d3fc616018d
Olle Månsson <31876997+ollema@users.noreply.github.com>
2025-01-28 10:49:12 +0100

    update config

---
+++ nvim/nvim/lua/config/keymaps.lua
@@ -0,0 +1,5 @@
+-- better movement
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
--- nvim/nvim/lua/config/options.lua
+++ nvim/nvim/lua/config/options.lua
@@ -4,10 +4,8 @@ vim.g.snacks_animate = false
 -- use fish as the shell for the integrated terminal
 vim.opt.shell = "fish"
 
--- use inline suggestions instead of completion engine
-vim.g.ai_cmp = false
-
 -- use basedpyright instead of pyright
 vim.g.lazyvim_python_lsp = "basedpyright"
+
 -- use ruff instead of ruff_lsp
 vim.g.lazyvim_python_ruff = "ruff"
